c84a35637ad3ac310ea762b87a93ba65521537b3
Rahul Krishna <i.m.ralk@gmail.com>
2020-07-15 16:00:42 -0400

    Autocommit from Makefile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,41 @@
-FROM rkrsn/cerno:base
+FROM ubuntu:18.04
 
-MAINTAINER Rahul Krishna <i.m.ralk@gmail.com>
+MAINTAINER Helpacksi <oo.helpacksi@gmail.com>
 
-WORKDIR /workspaces
+#----- Install packages -----
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    ocaml opam m4\
+    xz-utils \
+    vim \
+    curl \
+    openjdk-8-jdk       \
+    git maven bc        \
+    openssh-server wget \
+    build-essential     \
+    python-setuptools   \
+    python2.7           \
+    python2.7-dev       \
+    python-pip &&       \
+    pip install --upgrade pip virtualenv  && \
+    apt-get clean  && \
+    rm -rf /var/lib/apt/lists/*
 
-ADD . /workspaces/secern
 
+#----- INSTALL CLANG -----
+RUN curl -SL https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz | tar -xJC . && \
+RUN mv clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04 /LLVM_ROOT 
+
+#----- INSTALL PYTHON DEPENDENCIES-----
+RUN pip3 install pandas networkx tqdm matplotlib graphviz pydot
+
+# ----- This for debug only -----
+RUN wget -O ~/.vimrc https://gist.githubusercontent.com/rahlk/78d3d8f188a099dfd5114c35176e391d/raw/ed47d4fee79b5e177cb008abb319ebe4b542fc75/.vimrc
+RUN wget -O ~/.bashrc https://gist.githubusercontent.com/zachbrowne/8bc414c9f30192067831fafebd14255c/raw/322a762de6300c6afff4469c9c73b4dd8449d394/The%2520Ultimate%2520Bad%2520Ass%2520.bashrc%2520File
+
+#----- Set environment variable -----
+ENV PATH=/LLVM_ROOT/bin:/root/.opam/system/bin:$PATH
+ENV LD_LIBRARY_PATH=/LLVM_ROOT/lib
+ENV PYTHONDONTWRITEBYTECODE=1
 
 CMD ["sh -c bash"]
 
---
+++ secern-clang-cc/llvm_mode/afl-llvm-pass.so.cc
@@ -0,0 +1,160 @@
+/*
+  Copyright 2015 Google LLC All rights reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+/*
+   american fuzzy lop - LLVM-mode instrumentation pass
+   ---------------------------------------------------
+
+   Written by Laszlo Szekeres <lszekeres@google.com> and
+              Michal Zalewski <lcamtuf@google.com>
+
+   LLVM integration design comes from Laszlo Szekeres. C bits copied-and-pasted
+   from afl-as.c are Michal's fault.
+
+   This library is plugged into LLVM when invoking clang through afl-clang-fast.
+   It tells the compiler to add code roughly equivalent to the bits discussed
+   in ../afl-as.h.
+*/
+
+#define AFL_LLVM_PASS
+
+#include "../config.h"
+#include "../debug.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+using namespace llvm;
+using namespace std;
+
+namespace {
+
+  class AFLCoverage : public ModulePass {
+
+    public:
+
+      static char ID;
+      AFLCoverage() : ModulePass(ID) { }
+
+      bool runOnModule(Module &M) override;
+
+      // StringRef getPassName() const override {
+      //  return "American Fuzzy Lop Instrumentation";
+      // }
+
+  };
+
+}
+
+
+char AFLCoverage::ID = 0;
+
+bool AFLCoverage::runOnModule(Module &M) {
+
+  LLVMContext &C = M.getContext();
+
+  IntegerType *Int8Ty  = IntegerType::getInt8Ty(C);
+  IntegerType *Int32Ty = IntegerType::getInt32Ty(C);
+
+  string internals_prefix = "__"; //extern__";
+  string extern_func_prefix = "__extern__"; //extern__";
+
+  std::vector<Type*> paramTypes = {Type::getInt8Ty(C), Type::getInt8Ty(C)};
+  Type *retType = Type::getVoidTy(C);
+  FunctionType *preableType = FunctionType::get(retType, false);
+  FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
+  FunctionCallee preambleFunction = M.getOrInsertFunction("__extern__print_preamble", preableType);
+  FunctionCallee postambleFunction = M.getOrInsertFunction("__extern__print_postamble", preableType);
+  FunctionCallee logCallGraphEdge = M.getOrInsertFunction("__extern__print_callgraph_edge", logFuncType);
+
+  /* Instrument all the things! */
+
+  for (auto &F : M) {
+    // errs().write_escaped(F.getName()) << '\n';     
+    
+    const StringRef callerName = F.getName();
+    if (callerName.startswith(extern_func_prefix)) {
+          continue;
+    }    
+    
+    if (callerName.startswith(internals_prefix)) {
+          continue;
+    }    
+
+    if (F.isIntrinsic()) {
+          continue;
+    }    
+
+    if (!F.hasExactDefinition()) {
+          continue;
+    }    
+
+    if (F.getName() == "main") {
+      // print preamble and here
+      inst_iterator BI = inst_begin(F);
+      IRBuilder<> Preamble(&*(BI));
+      Preamble.CreateCall(preambleFunction, {});
+    }
+
+    for (auto &B : F){ 
+      for (auto &I :B) {
+        IRBuilder<> builder(&I);
+        Value *caller_strPointer = builder.CreateGlobalStringPtr(callerName);
+        /* Check to see if we have reached a 'call' instruction */
+        if (auto *CallInstr = dyn_cast<CallInst>(&(I))) {
+          Function *called_func = CallInstr->getCalledFunction();
+          if (!called_func || called_func->getName().startswith(extern_func_prefix) || called_func->getName().startswith(internals_prefix) || called_func->isIntrinsic() || !F.hasExactDefinition()) {
+            continue;
+          } else {
+            const StringRef calledName = called_func->getName();
+            BasicBlock::iterator insert_pt_above = builder.GetInsertPoint();
+            builder.SetInsertPoint(&B, insert_pt_above);
+            Value *called_strPointer = builder.CreateGlobalStringPtr(calledName);
+            errs() << callerName << "  " << calledName << '\n';
+            Value* args[] = {caller_strPointer, called_strPointer};
+            builder.CreateCall(logCallGraphEdge, args);
+          }
+        }
+      }
+    }
+  }
+  return true;
+}
+
+
+static void registerAFLPass(const PassManagerBuilder &,
+                            legacy::PassManagerBase &PM) {
+
+  PM.add(new AFLCoverage());
+
+}
+
+
+static RegisterStandardPasses RegisterAFLPass(
+    PassManagerBuilder::EP_ModuleOptimizerEarly, registerAFLPass);
+
+static RegisterStandardPasses RegisterAFLPass0(
+    PassManagerBuilder::EP_EnabledOnOptLevel0, registerAFLPass);
